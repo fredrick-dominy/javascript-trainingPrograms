6159839159ebba5b067f46ea1bfc254abfdf6656
Fredrick Dominy <fredrickdominy@gmail.com>
2014-01-28 10:50:28 -0700

    update - fixed bug if n was larger than array.length

--- 07-ExerciseInSumming.js
+++ 07-ExerciseInSumming.js
@@ -5,21 +5,27 @@
   // sum the n smallest integers in the array values (not necessarily ordered)
   // if n is 0 ... should return 0
 function minimumSum(values, n) {
-    values.sort(function(a,b) {
-      return a - b;
-    });
-    console.log(values); //test for sort
-    var addVal = 0;  //placeholder variable for the forloop
+  if (n>values.length){
+    var n = values.length;
+  }
+  values.sort(function(a,b) {
+    return a - b;
+  });
+  console.log(values); //test for sort
+  var addVal = 0;  //placeholder variable for the forloop
     for (var i = 0; i < n; i++) {
       console.log (i, values[i]);
       var addVal = addVal+values[i];
     };
-    console.log (addVal);  // test 
-    return addVal;
+  console.log (addVal);  // test 
+  return addVal;
 }
 // 
 //
 function maximumSum(values, n) {
+  if (n>values.length){
+      var n = values.length;
+    }
     values.sort(function(a,b) {
       return b - a;
     });
@@ -33,6 +39,6 @@ function maximumSum(values, n) {
     return addVal;
 }
 
-var values = [32,16,8,1,4,2,64];
-maximumSum(values, 2); // should return 64+32 = 96
-minimumSum(values, 2); // should return 1+2 = 3
+var values = [32,16,8,1];
+maximumSum(values, 5); // should return 64+32 = 96
+minimumSum(values, 5); // should return 1+2 = 3
