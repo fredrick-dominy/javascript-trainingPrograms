a921fb70f5488ba8147de79e8e98d93de3aaa2b6
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-06 16:15:45 -0700

    18 not sure what they want...

--- 18-returnToSanity.js
+++ 18-returnToSanity.js
@@ -1,9 +1,19 @@
 // Fix this code. 
 
+
+/*'use strict';
 function mystery() {
   var results =
     {sanity: 'Hello'};
   return
     results;
-}
+}*/
 
+console.log(mystery()); // returns undefined 
+
+// rewrite
+
+var mystery = function() { // this is a function mystery that ...
+  var results = {sanity: 'Hello'}; // creates a variable object property
+  return results;
+};
