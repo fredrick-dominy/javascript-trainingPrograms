1f2a652b532f7fb0f6e1ea31cb57ef1c039d9c73
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-06 15:34:32 -0700

    14 - passed
    wrapped the value:value statement in curly braces and
    quoted the property

--- 14-semiOptional.js
+++ 14-semiOptional.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // We have implemented a function wrap(value) that takes
 // a value of arbitrary type and wraps it in a new JavaScript
 // Object setting the 'value' key on the new Object to the 
@@ -6,9 +6,10 @@
 
 // Prior to attempt...
 
-function wrap(value) {
-  return
-  {
-    value:value 
-  };
-}
+var wrap = function(value) { // create a function wrap that takes a value ...
+  return {'value': value};  // and returns an object property
+};
+
+var wrappedObj = wrap("MyWrappedString"); // {}
+// and
+console.log(wrappedObj.value === "MyWrappedString");
