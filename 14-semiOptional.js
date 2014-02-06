9ac4bec254169766138713e2666eee5f94648323
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-06 15:00:13 -0700

    14 prior to solution

---
+++ 14-semiOptional.js
@@ -0,0 +1,14 @@
+'use strict'
+// We have implemented a function wrap(value) that takes
+// a value of arbitrary type and wraps it in a new JavaScript
+// Object setting the 'value' key on the new Object to the 
+// passed-in value
+
+// Prior to attempt...
+
+function wrap(value) {
+  return
+  {
+    value:value 
+  };
+}
