0e4d67a800d773b15e8c1f77191ddb0548c4441c
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-06 14:41:43 -0700

    13 1st attempt, PTS
    Prior To Submission

--- 13-Function3-MultiplyingTwoNumbers.js
+++ 13-Function3-MultiplyingTwoNumbers.js
@@ -2,3 +2,12 @@
 // needs to take two numbers as arguments, and
 // has to return the multiplication of the two 
 // arguments. 
+'use strict';
+var multiply = function(num1,num2) {
+  return num1 * num2;
+};
+
+console.log(multiply(32,35)); // 1120
+console.log(multiply(0,0)); // 0
+console.log(multiply("2", 4)); // 8
+console.log(multiply('bob', 2)); // NaN
