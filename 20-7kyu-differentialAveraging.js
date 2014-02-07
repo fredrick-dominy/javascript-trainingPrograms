78ccb670ef5a79f081a527347fef972e62abc888
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-06 20:28:18 -0700

    20 - prior to submission

---
+++ 20-7kyu-differentialAveraging.js
@@ -0,0 +1,11 @@
+'use strict';
+/*write a function that takes the *current* average, 
+the current number of ratings (data *points*) made, 
+and a new value to *add* to the average. */
+
+
+function addToAverage(current, points, add) {
+  // ...
+}
+
+addToAverage(0.5, 2, 1) // 0.666... (2 / 3) 
