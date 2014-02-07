e44adca27d439456a9a486d1cfc3d86260415b83
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-06 23:08:37 -0700

    21 - prior to submission
    build a calculator

--- 20-7kyu-differentialAveraging.js
+++ 20-7kyu-differentialAveraging.js
@@ -7,18 +7,11 @@ var AverageArray = [];
 var currentTally = 0;
 var runningTotal = 0;
 function addToAverage(current, points, add) {
-  // 1. push add to an array
-  // 2. display the average and print the array
-  // why do you need to input 'points'? Can't it automate that?
-  // why make a function where you manually have to input the current average?
-  // should this just be addToAverage(add) ??  
-  // then just output the climbing datapoint and changing average
   AverageArray.push(add);
   points += 1;
-  // calculate the average of the array...
   runningTotal += add;
   current = runningTotal/points;
-  console.log('The average is now '+current+', with '+points+' data points. Array = ['+AverageArray+']');
+  //console.log('The average is now '+current+', with '+points+' data points. Array = ['+AverageArray+']');
 }
 
 addToAverage(0, 0, 1);
