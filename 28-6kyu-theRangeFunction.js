ae01858e0b52a4bb348406b85c9005580a51147b
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-10 11:51:28 -0700

    28 - rewrite
    better logical control, but i misunderstood one of the expectations.

--- 28-6kyu-theRangeFunction.js
+++ 28-6kyu-theRangeFunction.js
@@ -10,10 +10,12 @@ Returns a list of integers from start to stop, incremented (or decremented) by s
 Note that ranges that stop before they start are considered to be zero-length instead of negative.
 
 */
+'use strict';
+/*
 function range(start, end, step) {
   var startFrom = start;
   //console.log(startFrom);
-  var endAt = end; //11
+  var endAt = end;
   if (endAt === 0) {
     return [];
   }
@@ -41,15 +43,81 @@ function range(start, end, step) {
   if (endAt > 0){
     for (var i = startFrom; i < endAt-jump; i+= jump) {
       rangeArray.push(startFrom += jump);
-    };
-} 
+    }
+}
 
   if (endAt < 0) {
     for (var i = startFrom; i > endAt-jump; i+=jump) {
-      rangeArray.push(startFrom += jump)
-    };
+      rangeArray.push(startFrom += jump);
+    }
   }
   return rangeArray;
-}
+} */
+//console.log(range(1,-4, 0));
+'use strict';
+var range = function(start,end,step) {
 
-console.log(range(1,-4, 0));
+// Set initial variables based on parameter input
+  if (start === 0 && end === undefined) {return [];}
+  var startFrom = (end === undefined) ? 0 : start;
+  var endAt = (end === undefined) ? start : end;
+  var jump = (step === undefined) ? 1 : Math.abs(step);
+  //console.log('The range starts here ...'+startFrom + ' And ends here...' + endAt);
+  //console.log('The step is ' + jump);
+  var rangeArray = [];
+
+  // Assert that the parameters must be an integer...
+  if (startFrom %1 !== 0 || endAt %1 !== 0 || jump %1 !== 0) {
+     //return console.log("END: Parameters need to be integers.");
+     return [];
+  }
+
+// if Positive endAt is < start, return an empty array.
+  if (endAt >= 0 && startFrom < endAt) {
+    if (startFrom > endAt) {
+      //console.log("END: This function cannot count down...");
+      return [];
+    } else {
+      // run the counter ... 
+      if (jump !== 0) {
+        for (var i = startFrom; i < endAt; i += jump) {
+          rangeArray.push(i);
+        }
+      } else if (jump === 0) {
+          for (var i = startFrom; i < endAt; i += 1) {
+            rangeArray.push(startFrom);
+          }
+        }
+      //console.log('END: ' + rangeArray);
+      return rangeArray;
+      }
+    }
+
+
+
+
+
+
+
+// if Negative endAt is > than startFrom, return an empty array
+  if (endAt <= 0 && startFrom > endAt) {
+    if (endAt > startFrom) {
+      //console.log ("END: Negative values cannot count back to zero");
+      return [];
+    } else {
+      // run the negative counter
+      if (jump !== 0){
+        for (var i = startFrom; i > endAt; i -= jump) {
+        rangeArray.push(i);
+        }
+      } else if (jump === 0) {
+        for (var i = startFrom; i > endAt; i -= 1) {
+          rangeArray.push(startFrom);
+        }
+      }
+      //console.log('END: ' + rangeArray);
+      return rangeArray;
+    }
+  }
+};
+console.log(range(10, 0));
