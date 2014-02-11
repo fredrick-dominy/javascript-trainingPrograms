9d69089adfa21f22488b3e69cb368c5011ab0269
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-11 15:45:45 -0700

    29 - updated instructions

--- 29-6kyu-countCharsString.js
+++ 29-6kyu-countCharsString.js
@@ -1,5 +1,17 @@
+/*
+
+The main idea is to count all the occuring ASCII
+ characters in string. If you have string like this
+  aba then the result should be 
+
+  { 'a': 2, 'b': 1 }
+
+What if the string is empty ? Then the result should 
+be empty object literal { }
+
+*/
+'use strict';
 var count = function(string) {
-  // return an object literal {} with empty string. 
   var stringObj = {
       input: string
     }
