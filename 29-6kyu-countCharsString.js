8264b3bdbad7ba793d0039ee795c005a64a908ac
Fredrick Dominy <fredrickdominy@gmail.com>
2014-02-11 09:42:15 -0700

    29 prior to submission

---
+++ 29-6kyu-countCharsString.js
@@ -0,0 +1,9 @@
+var count = function(string) {
+  // return an object literal {} with empty string. 
+  var stringObj = {
+      input: string
+    }
+    console.log(stringObj.input);
+  }
+  
+count('whooziwhatsit');
