4f77c51f1c2695bc396009c5afd534dac48dce27
Fredrick Dominy <fredrickdominy@gmail.com>
2014-06-17 18:48:17 -0700

    finished the exercise... however I found a great example of the solution at the bottom using string.replace()

--- 33-theVowelCode.js
+++ 33-theVowelCode.js
@@ -36,23 +36,23 @@ function encode(string){
 
   for (var i = 0; i < encStr.length; i += 1) {
 
-    if (encStr[i] == 'a' || encStr[i] == 'A') {
+    if (encStr[i] == 'a') {
       encStr.splice(i, 1, 1);
     }
 
-    if (encStr[i] == 'e' || encStr[i] == 'E') {
+    if (encStr[i] == 'e') {
       encStr.splice(i, 1, 2);
     }
 
-    if (encStr[i] == 'i' || encStr[i] == 'I') {
+    if (encStr[i] == 'i') {
       encStr.splice(i, 1, 3);
     }
 
-    if (encStr[i] == 'o' || encStr[i] == 'O') {
+    if (encStr[i] == 'o') {
       encStr.splice(i, 1, 4);
     }
 
-    if (encStr[i] == 'u' || encStr[i] == 'U') {
+    if (encStr[i] == 'u') {
       encStr.splice(i, 1, 5);
     }
 
@@ -66,9 +66,50 @@ function encode(string){
 //turn numbers back into vowels
 function decode(string){
 
+  var encStr = string.split('');
+  console.log('"' + string + '"' + ' was changed to ' + encStr);
 
+  for (var i = 0; i < encStr.length; i += 1) {
 
+    if (encStr[i] == 1 ) {
+      encStr.splice(i, 1, 'a');
+    }
+
+    if (encStr[i] == 2 ) {
+      encStr.splice(i, 1, 'e');
+    }
+
+    if (encStr[i] ==  3 ) {
+      encStr.splice(i, 1, 'i');
+    }
+
+    if (encStr[i] == 4 ) {
+      encStr.splice(i, 1, 'o');
+    }
+
+    if (encStr[i] == 5 ) {
+      encStr.splice(i, 1, 'u');
+    }
+
+  }
+  console.log('which ended up as ' + encStr);
+  var encoded = encStr.join('').toString();
+  console.log('Finally after a join and a toString, we have '+encoded);
+  return encoded;
 
 }
 
 encode('Pack my box with five dozen liquor jugs');
+decode('P1ck my b4x w3th f3v2 d4z2n l3q54r j5gs');
+
+
+// This is another person's solution using string.replace().... much better!
+// turn vowels into numbers
+function newEncode(string){
+  return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
+}
+
+//turn numbers back into vowels
+function newDecode(string){
+  return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
+}
