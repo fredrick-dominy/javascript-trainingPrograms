0dc60bcb6b8686762af726f6bf68cfa9bcbd6b45
Fredrick Dominy <fredrickdominy@gmail.com>
2014-06-22 09:14:32 -0700

    begun 36-6kyu-once.js  ... need some background research on this

---
+++ 36-6kyu-once.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Freximus on 6/22/2014.
+ * You'll implement once, a function that takes another function
+ * as an argument, and returns a new version of that function
+ * that can only be called once.
+ * Subsequent calls to the resulting function should have no effect
+ * (and should return undefined).
+ *
+ * For example:
+ *
+ * logOnce = once(console.log)
+ * logOnce("foo") // -> "foo"
+ * logOnce("bar") // -> no effect
+ *
+ */
+function once(fn) {
+  var count = 0;
+  if (count == 0) {
+    count += 1;
+    return fn;
+  } else {
+    console.log('Undefined');
+    return undefined;
+  }
+
+}
+
+function heyThere(){
+  console.log('Hi there')
+}
+
+once(heyThere());
+once(heyThere());
+once(heyThere());
